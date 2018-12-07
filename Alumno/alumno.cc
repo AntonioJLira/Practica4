afaadca2ef04417f2e2530730a08268027a7e57c
Antonio Jose Lira <i62liala@uco.es>
2018-12-07 18:00:19 +0100

    Creacion de la clase alumno y sus respectigos gets y sets

---
+++ Alumno/alumno.cc
@@ -0,0 +1,16 @@
+#include "alumno.h"
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+Alumno::Alumno(string nombre,string apellidos,string telefono,string direccion_postal,string DNI,string mayor_curso){
+
+          nombre_=nombre;
+          apellidos_=apellidos;
+          telefono_=telefono;
+          direccion_postal_=direccion_postal;
+          DNI_=DNI;
+          mayor_curso_=mayor_curso;
+
+    }
